82acc09fbf2674416a8cc5690de9b5a8ace85746
samaddy <addisonsamuel70@yahoo.com>
2023-05-02 12:46:25 -0400

    Add components to app

--- budget-app/src/App.tsx
+++ budget-app/src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React, { Component } from 'react'
 
-function App() {
+// export default class App extends Component {
+//   state = {
+//     count: 0
+//   }
+//   handleClick() {
+//     this.setState({ count: this.state.count + 1 })
+//   }
+//   render() {
+//     return (
+//       <div>App
+        
+//         <button onClick={this.handleClick.bind(this)}>
+//           Clicked {this.state.count}</button>
+//       </div>
+//     )
+//   }
+// }
+
+// // export default App
+
+import React, { useEffect, useState } from 'react'
+
+import Greetings from './component/Greetings'
+import Count from './component/Count'
+import Income from './component/Income'
+import Expense from './component/Expense'
+import Target from './component/Target'
+import Transfer from './component/Transfer'
+
+const App = () => {
+  const [count, setCount] = useState(0)
+  const handleClick = () => {
+    setCount(count + 1)
+  }
+  useEffect(() => {
+
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Greetings/>
+      Current count: {count}
+      <Count count={count} handleClick={handleClick} /> */}
+
+      <Income />
+      <Expense />
+      <Target />
+      <Transfer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
